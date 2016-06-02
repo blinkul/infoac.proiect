8065e2f1a6b201f7e0dfb641a2bfd735908f1376
blinkul <anghelciprian91@gmail.com>
2016-06-02 15:47:05 +0300

    test

--- src/abonamente/gui/AgendaFrame.java
+++ src/abonamente/gui/AgendaFrame.java
@@ -44,6 +44,7 @@ public class AgendaFrame extends javax.swing.JFrame {
         dtm = new DefaultTableModel();
         dtm.setColumnIdentifiers(new String[]{"ID","Nume", "Prenume", "CNP", "Numar de telefon"});
         tabelContacte.setModel(dtm);
+        tabelContacte.getColumnModel().getColumn(0).setPreferredWidth(10);
         afisareContacte();
         addListeners();
         //*******************************************************************************
@@ -267,15 +268,23 @@ public class AgendaFrame extends javax.swing.JFrame {
 
         tabelContacte.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "Nume", "Prenume", "CNP", "Numar de Telefon"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
         tabelContacte.setAlignmentX(0.0F);
         tabelContacte.setAlignmentY(0.0F);
         jScrollPane.setViewportView(tabelContacte);
