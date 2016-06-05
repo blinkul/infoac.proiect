1871faf3e70ae16e7d6ec4f81b61201554f2c9c0
blinkul <anghelciprian91@gmail.com>
2016-06-05 22:10:09 +0300

    Look And Feel

--- src/abonamente/gui/AgendaFrame.java
+++ src/abonamente/gui/AgendaFrame.java
@@ -599,7 +599,7 @@ public class AgendaFrame extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
